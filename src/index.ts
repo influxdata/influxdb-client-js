87be64b2de4d4e78947583f100b71036631aa7ee
Brandon Farmer <bthesorceror@gmail.com>
2019-01-23 15:39:11 -0800

    Add buckets

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
 import Tasks from "./wrappers/tasks"
 import Auth from "./wrappers/auth"
 import Links from "./wrappers/links"
+import Labels from "./wrappers/labels"
+import Buckets from "./wrappers/buckets"
 
 export default class Client {
   public links: Links
   public tasks: Tasks
   public auth: Auth
+  public labels: Labels
+  public buckets: Buckets
 
   constructor(basePath: string) {
     this.tasks = new Tasks(basePath)
     this.auth = new Auth(basePath)
     this.links = new Links(basePath)
+    this.labels = new Labels(basePath)
+    this.buckets = new Buckets(basePath)
   }
 }
---
+++ src/wrappers/buckets.ts
@@ -0,0 +1,40 @@
+import { BucketsApi, Organization, Bucket } from "../api";
+
+export default class {
+  private service: BucketsApi
+
+  constructor(basePath: string) {
+    this.service = new BucketsApi({basePath})
+  }
+
+  public async get(id: string): Promise<Bucket> {
+    const {data} = await this.service.bucketsBucketIDGet(id)
+
+    return data
+  }
+
+  public async getAllByOrg({name}: Organization): Promise<Bucket[]> {
+    const {data: {buckets}} = await this.service.bucketsGet(name)
+
+    return buckets || []
+  }
+
+  public async create({name}: Organization, bucket: Bucket): Promise<Bucket> {
+    const {data} = await this.service.bucketsPost(name, bucket)
+
+    return data
+  }
+
+  public async update(id: string, bucket: Partial<Bucket>): Promise<Bucket> {
+    const original = await this.get(id)
+    const {data} = await this.service.bucketsBucketIDPatch(id, {...original, ...bucket})
+
+    return data
+  }
+
+  public async delete(id: string): Promise<Response> {
+    const {data} = await this.service.bucketsBucketIDDelete(id)
+
+    return data
+  }
+}
