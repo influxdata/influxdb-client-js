04857c8893345d75630f78fda1f1c65653b05257
Brandon Farmer <bthesorceror@gmail.com>
2019-01-23 16:39:55 -0800

    Add telegraf configs

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import Labels from "./wrappers/labels"
 import Buckets from "./wrappers/buckets"
 import Organizations from "./wrappers/organizations"
 import Scrapers from "./wrappers/scrapers"
+import TelegrafConfigs from "./wrappers/telegrafConfigs"
 
 export default class Client {
   public links: Links
@@ -14,6 +15,7 @@ export default class Client {
   public buckets: Buckets
   public organizations: Organizations
   public scrapers: Scrapers
+  public telegrafConfigs: TelegrafConfigs
 
   constructor(basePath: string) {
     this.tasks = new Tasks(basePath)
@@ -23,5 +25,6 @@ export default class Client {
     this.buckets = new Buckets(basePath)
     this.organizations = new Organizations(basePath)
     this.scrapers = new Scrapers(basePath)
+    this.telegrafConfigs = new TelegrafConfigs(basePath)
   }
 }
---
+++ src/wrappers/telegrafConfigs.ts
@@ -0,0 +1,37 @@
+import { TelegrafsApi, Organization, TelegrafPluginConfig } from "../api";
+
+export default class {
+  private service: TelegrafsApi
+
+  constructor(basePath: string) {
+    this.service = new TelegrafsApi({basePath})
+  }
+
+  public async getAllByOrg(org: Organization): Promise<TelegrafPluginConfig[]> {
+    if (!org.id) {
+      throw new Error('organization must have an id')
+    }
+
+    const {data: {configurations}} = await this.service.telegrafsGet(org.id)
+
+    return configurations || []
+  }
+
+  public async getTOML(id: string): Promise<string> {
+    const options = {
+      headers: {
+        Accept: 'application/toml'
+      }
+    }
+
+    const {data} = await this.service.telegrafsTelegrafIDGet(id, options)
+
+    return data as string
+  }
+
+  public async delete(id: string): Promise<Response> {
+    const {data} = await this.service.telegrafsTelegrafIDDelete(id)
+
+    return data as Response
+  }
+}
