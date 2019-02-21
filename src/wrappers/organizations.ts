9da2e426a2345c3921c87a001c2c403d929d8782
Palak Bhojani <palak@influxdata.com>
2019-02-21 14:41:32 -0800

    Update orgs wrapper to include owners post api call

--- src/wrappers/organizations.ts
+++ src/wrappers/organizations.ts
@@ -1,52 +1,79 @@
-import { Organization, OrganizationsApi, ResourceMember, ResourceOwner } from "../api";
+import {
+  AddResourceMemberRequestBody,
+  Organization,
+  OrganizationsApi,
+  ResourceMember,
+  ResourceOwner,
+} from "../api";
 
 export default class {
   private service: OrganizationsApi;
 
   constructor(basePath: string) {
-    this.service = new OrganizationsApi({basePath});
+    this.service = new OrganizationsApi({ basePath });
   }
 
   public async get(id: string): Promise<Organization> {
-    const {data} = await this.service.orgsOrgIDGet(id);
+    const { data } = await this.service.orgsOrgIDGet(id);
 
     return data;
   }
 
   public async getAll(): Promise<Organization[]> {
-    const {data: {orgs}} = await this.service.orgsGet();
+    const {
+      data: { orgs },
+    } = await this.service.orgsGet();
 
     return orgs || [];
   }
 
   public async create(org: Organization): Promise<Organization> {
-    const {data} = await this.service.orgsPost(org);
+    const { data } = await this.service.orgsPost(org);
 
     return data;
   }
 
   public async delete(id: string): Promise<Response> {
-    const {data} = await this.service.orgsOrgIDDelete(id);
+    const { data } = await this.service.orgsOrgIDDelete(id);
 
     return data;
   }
 
-  public async update(id: string, org: Partial<Organization>): Promise<Organization> {
+  public async update(
+    id: string,
+    org: Partial<Organization>,
+  ): Promise<Organization> {
     const original = await this.get(id);
-    const {data} = await this.service.orgsOrgIDPatch(id, {...original, ...org});
+    const { data } = await this.service.orgsOrgIDPatch(id, {
+      ...original,
+      ...org,
+    });
 
     return data;
   }
 
   public async members(id: string): Promise<ResourceMember[]> {
-    const {data: {users}} = await this.service.orgsOrgIDMembersGet(id);
+    const {
+      data: { users },
+    } = await this.service.orgsOrgIDMembersGet(id);
 
     return users || [];
   }
 
   public async owners(id: string): Promise<ResourceOwner[]> {
-    const {data: {users}} = await this.service.orgsOrgIDOwnersGet(id);
+    const {
+      data: { users },
+    } = await this.service.orgsOrgIDOwnersGet(id);
 
     return users || [];
+  }
+
+  public async createOwner(
+    id: string,
+    user: AddResourceMemberRequestBody,
+  ): Promise<ResourceOwner> {
+    const { data } = await this.service.orgsOrgIDOwnersPost(id, user);
+
+    return data;
   }
 }
