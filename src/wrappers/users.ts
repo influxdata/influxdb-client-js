815f1065cc8dbcffdf997ac97c1aa448c23ac8c1
Palak Bhojani <palak@influxdata.com>
2019-03-04 15:11:00 -0800

    Update users api to include a get call for all users

--- src/wrappers/users.ts
+++ src/wrappers/users.ts
@@ -1,14 +1,20 @@
-import { User, UsersApi } from "../api";
+import { User, Users, UsersApi } from "../api";
 
 export default class {
   private service: UsersApi;
 
   constructor(basePath: string) {
-    this.service = new UsersApi({basePath});
+    this.service = new UsersApi({ basePath });
   }
 
   public async me(): Promise<User> {
-    const {data} = await this.service.meGet();
+    const { data } = await this.service.meGet();
+
+    return data;
+  }
+
+  public async getAllUsers(): Promise<Users> {
+    const { data } = await this.service.usersGet();
 
     return data;
   }
