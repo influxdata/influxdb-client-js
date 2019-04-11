d7de8c08cac900c34a59ab4591312b5a2620a89e
Andrew Watkins <watts@influxdb.com>
2019-04-11 11:24:47 -0700

    chore: manual ast types (#96)
    * chore: organize types index
    
    * chore: override swagger ast types with local ast types

--- dist/types/index.js
+++ dist/types/index.js
@@ -1,11 +1,6 @@
 "use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
 Object.defineProperty(exports, "__esModule", { value: true });
-var TemplateType;
-(function (TemplateType) {
-    TemplateType["Label"] = "label";
-    TemplateType["Task"] = "task";
-    TemplateType["Dashboard"] = "dashboard";
-    TemplateType["View"] = "view";
-    TemplateType["Cell"] = "cell";
-    TemplateType["Variable"] = "variable";
-})(TemplateType = exports.TemplateType || (exports.TemplateType = {}));
+__export(require("./templates"));
