125e5c38d85eaba4984a4651c2cf40cedd045c1b
Brandon Farmer <bthesorceror@gmail.com>
2019-04-24 13:20:51 -0700

    Update examples

--- src/examples/write.ts
+++ src/examples/write.ts
@@ -1,7 +1,7 @@
 import Client from '../index'
 
 const token = process.env.INFLUXDB_TOKEN
-const client = new Client('http://localhost:9999/api/v2', token)
+const client = new Client('http://127.0.0.1:9999/api/v2', token)
 
 const data = ''
 const bucket = 'bucket'
