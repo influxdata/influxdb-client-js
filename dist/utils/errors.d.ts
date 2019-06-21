52905e695cfe3a7455e4d71c0868d913ef2d1731
Christopher Henn <chris@chrishenn.net>
2019-06-21 11:59:31 -0700

    Cleanup build process
    - Add rollup configuration so that consumers do not need to use a
      bundler
    - Remove built assets from source
    - Do not mix default and named exports, which creates problems for
      UMD and CommonJS consumers
    - Add CircleCI integration
    - Switch to Yarn

--- dist/utils/errors.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class CancellationError extends Error {
-    constructor(...args: any[]);
-}
-export declare class LargeResponseError extends Error {
-    constructor(...args: any[]);
-}
