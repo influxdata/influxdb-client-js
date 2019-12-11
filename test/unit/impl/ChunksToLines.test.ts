d2c270dee98c4e7c5552a75bf9f594d81d9e4136
Pavel Zavora <pavel.zavora@bonitoo.io>
2019-12-11 15:59:40 +0100

    feat: add line splitter from Buffer chunks

---
+++ test/unit/impl/ChunksToLines.test.ts
@@ -0,0 +1,71 @@
+import {expect} from 'chai'
+import ChunksToLines from '../../../src/impl/ChunksToLines'
+import chunksToLinesTables from '../../fixture/chunksToLinesTables.json'
+import {CommunicationObserver, Cancellable} from '../../../src/transport'
+import sinon from 'sinon'
+
+interface ChunkTest {
+  name: string
+  chunks: string[]
+  lines: string[]
+  withCancellable?: boolean // use observer with cancellable implementation
+}
+
+class CollectLinesObserver implements CommunicationObserver<string> {
+  lines: string[] = []
+  completed = 0
+  failed = 0
+  cancellableSet = false
+
+  next(data: string): void {
+    this.lines.push(data)
+  }
+  error(error: Error): void {
+    this.failed++
+  }
+  complete(): void {
+    this.completed++
+  }
+}
+
+class CollectLinesObserver2 extends CollectLinesObserver {
+  useCancellable(cancellable: Cancellable): void {
+    this.cancellableSet = true
+  }
+}
+
+describe('ChunksToLines', () => {
+  ;(chunksToLinesTables as Array<ChunkTest>).forEach((test: ChunkTest) => {
+    it(`creates correct lines from test set '${test.name}'`, () => {
+      const target = test.withCancellable
+        ? new CollectLinesObserver2()
+        : new CollectLinesObserver()
+      const subject = new ChunksToLines(target)
+      subject.useCancellable({isCancelled: sinon.mock(), cancel: sinon.mock()})
+      let failed = false
+      for (let i = 0; i < test.chunks.length; i++) {
+        const chunk = test.chunks[i]
+        if (chunk === 'error') {
+          failed = true
+          subject.error(new Error())
+        } else {
+          subject.next(Buffer.from(chunk, 'utf8'))
+          if (failed) {
+            subject.error(new Error())
+          }
+        }
+      }
+      subject.complete()
+      expect(test.lines).to.deep.equal(target.lines)
+      expect(target.cancellableSet).to.equal(!!test.withCancellable)
+      expect(target.failed).to.equal(failed ? 1 : 0)
+      expect(target.completed).to.equal(failed ? 0 : 1)
+    })
+  })
+  it('fails on unsupported data', () => {
+    const target = new CollectLinesObserver()
+    const subject = new ChunksToLines(target)
+    subject.next(1)
+    expect(target.failed).to.be.equal(1)
+  })
+})
