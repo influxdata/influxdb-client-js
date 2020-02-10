5bd1a8d299334fe52d9472e3f7a045672c4bcf57
Pavel Zavora <pavel.zavora@bonitoo.io>
2020-02-10 12:24:29 +0100

    feat(examples): add INFLUXDB_ORG env variable

--- examples/env.js
+++ examples/env.js
@@ -3,7 +3,7 @@ const url = process.env['INFLUXDB_URL'] || 'http://localhost:9999'
 /** InfluxDB authorization token */
 const token = process.env['INFLUXDB_TOKEN'] || 'my-token'
 /** Organization within InfluxDB URL  */
-const org = 'my-org'
+const org = process.env['INFLUXDB_ORG'] || 'my-org'
 /**InfluxDB bucket used in examples  */
 const bucket = 'my-bucket'
 // ONLY onboarding example
