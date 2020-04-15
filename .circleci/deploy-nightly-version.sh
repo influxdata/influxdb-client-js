955414bb5c2e987cf8f38bcbb2ef63e652b74264
Jakub Bednar <jakub.bednar@gmail.com>
2020-04-15 10:41:59 +0200

    fix: building nightly packages on CircleCI

--- .circleci/deploy-nightly-version.sh
+++ .circleci/deploy-nightly-version.sh
@@ -1,7 +1,24 @@
+set -e
+
+SCRIPT_PATH="$( cd "$(dirname "$0")" || exit ; pwd -P )"
+
+# Update Version
 VERSION=$(cat packages/core/src/impl/version.ts | sed 's/[^0-9.]*//g' | awk -F. '{$2+=1; OFS="."; print $1"."$2"."$3}')
 sed -i -e "s/CLIENT_LIB_VERSION = '.*'/CLIENT_LIB_VERSION = '${VERSION}.nightly'/" packages/core/src/impl/version.ts
 git config user.name "CircleCI Builder"
 git config user.email "noreply@influxdata.com"
 git commit -am "chore(release): prepare to release influxdb-client-js-${VERSION}.nightly"
-yarn run build
+
+# Build Core
+cd "${SCRIPT_PATH}"/../packages/core
+npm install
+yarn add -D @typescript-eslint/eslint-plugin
+yarn build
+
+# Build Apis
+cd "${SCRIPT_PATH}"/../packages/apis
+npm install
+yarn build
+
+# Publish
 yarn lerna publish --canary preminor --no-git-tag-version --force-publish --preid nightly --yes
