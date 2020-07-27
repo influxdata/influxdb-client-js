e275bc79c83f26d5a0eef3f632d0b893b44f01da
Pavel Zavora <pavel.zavora@bonitoo.io>
2020-07-27 07:46:52 +0200

    docs: add documentation and remarks to client packages

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,28 @@
+/**
+ * The `@influxdata/influxdb-client` package provides optimized APIs that write or query InfluxDB v2.
+ *
+ * @remarks
+ * The entry point of this package is the {@link @influxdata/influxdb-client#InfluxDB } class. It is
+ * initialized with options that tells how to communicate with InfluxDB. The simple usage pattern is:
+ *
+ * ```
+ * import {InfluxDB} = from('@influxdata/influxdb-client')
+ * const influxDB = new InfluxDB({url: "http://localhost:9999", token: "your-api-token"})
+ * ```
+ *
+ * The influxDB object let you create two essential API instances, {@link @influxdata/influxdb-client#InfluxDB.getWriteApi }
+ * and {@link @influxdata/influxdb-client#InfluxDB.getQueryApi }. The {@link @influxdata/influxdb-client#WriteApi}
+ * asynchronously writes measurement points on background, in batches to optimize network traffic, and with retries
+ * upon failures. The {@link @influxdata/influxdb-client#QueryApi} let you execute a flux query against InfluxDB
+ * and offers several ways to stream query results.
+ *
+ * The influxDB object is also used to create more specialized InfluxDB management API instances in
+ * {@link @influxdata/influxdb-client-apis# }
+ *
+ * See also {@link https://github.com/influxdata/influxdb-client-js/tree/master/examples | examples} to know more.
+ *
+ * @packageDocumentation
+ */
 export * from './options'
 export * from './errors'
 export * from './util/escape'
