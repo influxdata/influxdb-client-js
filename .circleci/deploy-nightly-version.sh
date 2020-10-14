b2dd3215ab8b45e22180e7b75fac70baf428a58b
Jakub Bednář <jakub.bednar@gmail.com>
2020-10-14 12:41:52 +0200

    fix: deploying nightly packages (#268)

--- .circleci/deploy-nightly-version.sh
+++ .circleci/deploy-nightly-version.sh
@@ -5,6 +5,7 @@ SCRIPT_PATH="$( cd "$(dirname "$0")" || exit ; pwd -P )"
 # Update Version
 VERSION=$(cat packages/core/src/impl/version.ts | sed 's/[^0-9.]*//g' | awk -F. '{$2+=1; OFS="."; print $1"."$2"."$3}')
 sed -i -e "s/CLIENT_LIB_VERSION = '.*'/CLIENT_LIB_VERSION = '${VERSION}.nightly'/" packages/core/src/impl/version.ts
+yarn lerna version "$VERSION"-nightly."$CIRCLE_BUILD_NUM" --no-git-tag-version --yes
 git config user.name "CircleCI Builder"
 git config user.email "noreply@influxdata.com"
 git commit -am "chore(release): prepare to release influxdb-client-js-${VERSION}.nightly"
@@ -14,4 +15,4 @@ cd "${SCRIPT_PATH}"/..
 yarn build
 
 # Publish
-yarn lerna publish --canary preminor --no-git-tag-version --force-publish --preid nightly --yes
+yarn lerna publish --canary from-package --no-git-tag-version --force-publish --preid nightly --yes
