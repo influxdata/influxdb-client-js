9f753f7a9253e10b03c85b5b4224e247f152871a
Jakub Bednar <jakub.bednar@gmail.com>
2020-11-02 08:29:36 +0100

    fix: deploying nightly browser package

--- .circleci/deploy-nightly-version.sh
+++ .circleci/deploy-nightly-version.sh
@@ -5,7 +5,7 @@ SCRIPT_PATH="$( cd "$(dirname "$0")" || exit ; pwd -P )"
 # Update Version
 VERSION=$(cat packages/core/src/impl/version.ts | sed 's/[^0-9.]*//g' | awk -F. '{$2+=1; OFS="."; print $1"."$2"."$3}')
 sed -i -e "s/CLIENT_LIB_VERSION = '.*'/CLIENT_LIB_VERSION = '${VERSION}.nightly'/" packages/core/src/impl/version.ts
-yarn lerna version "$VERSION"-nightly."$CIRCLE_BUILD_NUM" --no-git-tag-version --yes
+yarn lerna version "$VERSION"-nightly."$CIRCLE_BUILD_NUM" --no-git-tag-version --force-publish --yes
 git config user.name "CircleCI Builder"
 git config user.email "noreply@influxdata.com"
 git commit -am "chore(release): prepare to release influxdb-client-js-${VERSION}.nightly"
