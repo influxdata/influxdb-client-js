32c573daf72ce6338c0363105c5210cbc715c771
Pavel Zavora <pavel.zavora@bonitoo.io>
2021-08-23 14:25:12 +0200

    fix(examples): revert query.ts to 1.15

--- examples/query.ts
+++ examples/query.ts
@@ -1,32 +1,74 @@
 #!./node_modules/.bin/ts-node
-const {InfluxDB} = require('@influxdata/influxdb-client')
+//////////////////////////////////////////
+// Shows how to use InfluxDB query API. //
+//////////////////////////////////////////
 
-// You can generate a Token from the "Tokens Tab" in the UI
-const token = 'my-token'
-const org = 'my-org'
+import {InfluxDB, FluxTableMetaData} from '@influxdata/influxdb-client'
+import {url, token, org} from './env'
 
-const client = new InfluxDB({url: 'http://localhost:8086', token: token})
+const queryApi = new InfluxDB({url, token}).getQueryApi(org)
+const fluxQuery =
+  'from(bucket:"my-bucket") |> range(start: -1d) |> filter(fn: (r) => r._measurement == "temperature")'
 
-const queryApi = client.getQueryApi(org)
-
-const query = `option v = {timeRangeStart: -30d, timeRangeStop: now()}
-
-from(bucket: "my-bucket")
-	|> range(start: v.timeRangeStart, stop: v.timeRangeStop)
-	|> filter(fn: (r) =>
-		(r["_measurement"] == "m2m"))
-	|> filter(fn: (r) =>
-		(r["_field"] == "field"))`
-queryApi.queryRows(query, {
-  next(row, tableMeta) {
+console.log('*** QUERY ROWS ***')
+// Execute query and receive table metadata and rows.
+// https://v2.docs.influxdata.com/v2.0/reference/syntax/annotated-csv/
+queryApi.queryRows(fluxQuery, {
+  next(row: string[], tableMeta: FluxTableMetaData) {
     const o = tableMeta.toObject(row)
-    console.log(`${o._time} ${o._measurement}: ${o._field}=${o._value}`)
+    // console.log(JSON.stringify(o, null, 2))
+    console.log(
+      `${o._time} ${o._measurement} in '${o.location}' (${o.example}): ${o._field}=${o._value}`
+    )
   },
-  error(error) {
+  error(error: Error) {
     console.error(error)
-    console.log('Finished ERROR')
+    console.log('\nFinished ERROR')
   },
   complete() {
-    console.log('Finished SUCCESS')
+    console.log('\nFinished SUCCESS')
   },
 })
+
+// // Execute query and return the whole result as a string.
+// // Use with caution, it copies the whole stream of results into memory.
+// queryApi
+//   .queryRaw(fluxQuery)
+//   .then(result => {
+//     console.log(result)
+//     console.log('\nQueryRaw SUCCESS')
+//   })
+//   .catch(error => {
+//     console.error(error)
+//     console.log('\nQueryRaw ERROR')
+//   })
+
+// // Execute query and collect result rows in a Promise.
+// // Use with caution, it copies the whole stream of results into memory.
+// queryApi
+//   .collectRows(fluxQuery /*, you can specify a row mapper as a second arg */)
+//   .then(data => {
+//     data.forEach(x => console.log(JSON.stringify(x)))
+//     console.log('\nCollect ROWS SUCCESS')
+//   })
+//   .catch(error => {
+//     console.error(error)
+//     console.log('\nCollect ROWS ERROR')
+//   })
+
+// Execute query and receive result lines in annotated csv format
+// queryApi.queryLines(
+//   fluxQuery,
+//   {
+//     error(error: Error) {
+//       console.error(error)
+//       console.log('\nFinished ERROR')
+//     },
+//     next(line: string) {
+//       console.log(line)
+//     },
+//     complete() {
+//       console.log('\nFinished SUCCESS')
+//     },
+//   }
+// )
