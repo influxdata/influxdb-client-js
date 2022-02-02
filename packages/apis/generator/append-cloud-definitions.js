a3299c5951b680210ffe7fa233f838ce2387cf28
Jakub Bednar <jakub.bednar@gmail.com>
2022-02-02 09:51:26 +0100

    chore: return modified swagger doc

--- packages/apis/generator/append-cloud-definitions.js
+++ packages/apis/generator/append-cloud-definitions.js
@@ -1,11 +1,11 @@
-const swaggerParser = require('swagger-parser')
-
-exports.patch = async doc => {
-  const cloudApi = await swaggerParser.bundle('./resources/cloud.yml')
+exports.patch = async (doc, swaggerParser) => {
+  const cloudApi = await swaggerParser('./resources/cloud.yml')
   const cloudTypes =
     cloudApi.components.schemas['Resource']['properties']['type']
   const ossTypes = doc.components.schemas['Resource']['properties']['type']
   ossTypes.enum = ossTypes.enum.concat(
     cloudTypes.enum.filter(cloudType => !ossTypes.enum.includes(cloudType))
   )
+
+  return doc
 }
