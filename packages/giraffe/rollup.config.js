15ef44564fe4a401b8d9ac0e24b543bcff5293bf
Pavel Zavora <pavel.zavora@bonitoo.io>
2022-07-18 08:02:12 +0200

    fix(build): rely upon rollup resolve exports support

--- packages/giraffe/rollup.config.js
+++ packages/giraffe/rollup.config.js
@@ -23,7 +23,7 @@ function createConfig({format, out, name, target, noTerser}) {
       }),
       // @influxdata/influxdb-client (core) package uses `module:browser`
       // property in its package.json to point to an ES module created for the browser
-      resolve({mainFields: ['module:browser']}),
+      resolve({browser: true}),
       noTerser ? undefined : terser(),
       gzip(),
     ],
