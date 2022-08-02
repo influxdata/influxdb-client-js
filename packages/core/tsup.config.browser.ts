63375acce370ce76fc8bb0928225d2d99bb2e83a
Pavel Zavora <pavel.zavora@bonitoo.io>
2022-08-02 11:38:24 +0200

    chore: emulate UMD output in esbuild

--- packages/core/tsup.config.browser.ts
+++ packages/core/tsup.config.browser.ts
@@ -26,6 +26,21 @@ export default defineConfig({
   esbuildOptions(options, {format}) {
     options.outdir = undefined
     options.outfile = outFiles[format]
+    if (format === 'cjs') {
+      // esbuild does not generate UMD format OOTB, see https://github.com/evanw/esbuild/issues/507
+      // the following code is a trick to generate UMD output in place of cjs
+      options.format = 'iife'
+      options.banner = {
+        js: `(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+  typeof define === 'function' && define.amd ? define(['exports'], factory) :
+  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global["@influxdata/influxdb-client"] = {}));
+})(this, (function (exports) {`,
+      }
+      options.footer = {
+        js: `Object.defineProperty(exports, '__esModule', { value: true });Object.assign(exports, ${options.globalName});}));`,
+      }
+    }
   },
   esbuildPlugins: [
     {
