e528b2859dd7efed25a710e74141b92669bfd109
Pavel Zavora <pavel.zavora@bonitoo.io>
2022-08-03 10:47:39 +0200

    chore: repair giraffe UMD build

--- packages/giraffe/tsup.config.ts
+++ packages/giraffe/tsup.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
         js: `(function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
   typeof define === 'function' && define.amd ? define(['exports'], factory) :
-  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global["@influxdata/influxdb-client"] = {}));
+  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global["@influxdata/influxdb-client-giraffe"] = {}));
 })(this, (function (exports) {`,
       }
       options.footer = {
