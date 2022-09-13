48cae63645a3a387e79a0afc6c89bcd3653e52c1
Pavel Zavora <pavel.zavora@bonitoo.io>
2022-09-13 07:56:52 +0200

    fix: detect and repair wrong documentation references

--- scripts/fix-extracted-api-files.js
+++ scripts/fix-extracted-api-files.js
@@ -2,6 +2,12 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path')
 const fs = require('fs')
+const referenceIds = {}
+
+const ignoredReferencePackages = [
+  '!' /* ts&js builtin types */,
+  '@influxdata/giraffe!' /* giraffe references */,
+]
 
 const markdownLinkRE = /\[([^\]]*)\]\(([^)]*)\)/g
 function changeMarkdownLinks(text) {
@@ -9,16 +15,33 @@ function changeMarkdownLinks(text) {
     // changes markdown style [text](link) to tsdoc {@link URL | text }
     return text.replace(markdownLinkRE, (match, text, link) => {
       const retVal = `{@link ${link} | ${text} }`
-      console.log(` ${match} => ${retVal}`)
+      console.log(` CHANGED ${match} => ${retVal}`)
       return retVal
     })
   }
   return text
 }
 
-function replaceInExtractorFile(file) {
-  const data = fs.readFileSync(file, 'utf8')
-  const json = JSON.parse(data)
+function storeCanonicalReferences(json, references) {
+  function walk(obj) {
+    if (typeof obj === 'object') {
+      if (Array.isArray(obj)) {
+        obj.forEach(walk)
+      } else {
+        const canonicalReference = obj['canonicalReference']
+        if (canonicalReference && obj['kind'] && obj['kind'] !== 'Reference') {
+          references[canonicalReference] = true
+        }
+        for (const key in obj) {
+          walk(obj[key])
+        }
+      }
+    }
+  }
+  walk(json)
+}
+
+function fixExtractedFile(file, json, errors = []) {
   console.log(`correct: ${file}`)
 
   function replaceObject(obj) {
@@ -28,14 +51,22 @@ function replaceInExtractorFile(file) {
       } else {
         if (obj['kind'] === 'Reference') {
           const canonicalReference = obj['canonicalReference']
-          if (canonicalReference.indexOf('!default:') > 0) {
-            const text = obj['text']
-            const replaced = canonicalReference.replace(
-              /!(default):/,
-              `!${text}:`
-            )
-            console.log(` ${canonicalReference} => ${replaced}`)
-            obj.canonicalReference = replaced
+          if (canonicalReference && !referenceIds[canonicalReference]) {
+            if (canonicalReference.indexOf('!~') > 0) {
+              const replaced = canonicalReference.replace('!~', '!')
+              console.log(` FIXED ${canonicalReference} => ${replaced}`)
+              obj.canonicalReference = replaced
+              return
+            }
+            // report unresolved reference
+            for (const ignoredPackage of ignoredReferencePackages) {
+              if (canonicalReference.startsWith(ignoredPackage)) {
+                return
+              }
+            }
+            const msg = ` MISSING ${canonicalReference}`
+            errors.push(`${file}: ${msg}`)
+            console.log(msg)
           }
         } else {
           for (const key in obj) {
@@ -56,6 +87,21 @@ function replaceInExtractorFile(file) {
 
 const docsDir = path.resolve(__dirname, '..', 'docs')
 const files = fs.readdirSync(docsDir).filter((x) => /\.api\.json$/.test(x))
-files.forEach((file) => {
-  replaceInExtractorFile(path.resolve(docsDir, file))
-})
+const fileToJson = files.reduce((acc, file) => {
+  file = path.resolve(docsDir, file)
+  const json = JSON.parse(fs.readFileSync(file, 'utf8'))
+  acc[file] = json
+  storeCanonicalReferences(json, referenceIds)
+  return acc
+}, {})
+
+const errors = []
+for (const [file, json] of Object.entries(fileToJson)) {
+  fixExtractedFile(file, json, errors)
+}
+
+if (errors.length) {
+  console.error(`\n\nERRORS:`)
+  errors.forEach((e) => console.error(e))
+  process.exit(1)
+}
