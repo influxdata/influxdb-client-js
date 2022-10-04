bbb4763c6d1e5c74fcb318695bffbc3356c5926a
Pavel Zavora <pavel.zavora@bonitoo.io>
2022-10-04 06:37:45 +0200

    feat(examples): change deno example to use for-await loop

--- examples/query.deno.ts
+++ examples/query.deno.ts
@@ -3,10 +3,7 @@
 // A modified query.ts example that works with deno //
 //////////////////////////////////////////////////////
 
-import {
-  FluxTableMetaData,
-  InfluxDB,
-} from 'https://cdn.skypack.dev/@influxdata/influxdb-client-browser?dts'
+import {InfluxDB} from 'https://cdn.skypack.dev/@influxdata/influxdb-client-browser?dts'
 
 const url = 'http://localhost:8086'
 const token = 'my-token'
@@ -17,19 +14,16 @@ const fluxQuery =
   'from(bucket:"my-bucket" ) |> range(start: 0) |> filter(fn: (r) => r._measurement == "temperature")'
 
 console.log('** QUERY ROWS ***')
-queryApi.queryRows(fluxQuery, {
-  next(row: string[], tableMeta: FluxTableMetaData) {
-    const o = tableMeta.toObject(row)
+try {
+  for await (const {values, tableMeta} of queryApi.iterateRows(fluxQuery)) {
+    const o = tableMeta.toObject(values)
     // console.log(JSON.stringify(o, null, 2))
     console.log(
       `${o._time} ${o._measurement} in '${o.location}' (${o.example}): ${o._field}=${o._value}`
     )
-  },
-  error(error: Error) {
-    console.error(error)
-    console.log('\nFinished ERROR')
-  },
-  complete() {
-    console.log('\nFinished SUCCESS')
-  },
-})
+  }
+  console.log('\nFinished SUCCESS')
+} catch (error) {
+  console.error(error)
+  console.log('\nFinished ERROR')
+}
