f1fae82853f2237a67ada35e513d97807c40abe8
NguyenHoangSon96 <hoangsonnguyen4321@gmail.com>
2026-05-29 18:21:51 +0700

    refactor: migrate to new eslint config structure for apis/

--- packages/apis/eslint.config.mjs
+++ packages/apis/eslint.config.mjs
@@ -1,10 +1,19 @@
-import { defineConfig } from "eslint/config";
+import {defineConfig} from 'eslint/config'
+import tsdoc from 'eslint-plugin-tsdoc'
+import tsParser from '@typescript-eslint/parser'
 
-export default defineConfig([{
-    files: ["src/generated/*.ts"],
-    ignores: ["**/*.test.js"],
-
-    rules: {
-        "tsdoc/syntax": "warn",
+export default defineConfig([
+  {
+    files: ['src/generated/*.ts'],
+    ignores: ['**/*.test.js'],
+    plugins: {
+      tsdoc,
     },
-}]);
+    languageOptions: {
+      parser: tsParser,
+    },
+    rules: {
+      'tsdoc/syntax': 'warn',
+    },
+  },
+])
